07e08d6f10751dc380376b95d0ed25aa263ede52
cirill98 <100434317+cirill98@users.noreply.github.com>
2022-08-25 20:08:23 +0500

    Dz_6

---
+++ dz_6_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Сколько цифр будет в массиве");
+int num = Convert.ToInt32(Console.ReadLine());
+int total = 0;
+for (int i = 0; i < num; i++)
+{
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n > 0)
+    {
+        total += 1;
+    }
+}
+Console.WriteLine(total);
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
